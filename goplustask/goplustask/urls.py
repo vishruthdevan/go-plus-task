dc685a43049a477bc894318e8ca4ef5964a1ce1a
Vishruth Devan <vishruthdevan@gmail.com>
2023-01-21 16:16:25 +0530

    feat: create model to store likes and dislikes

--- goplustask/goplustask/urls.py
+++ goplustask/goplustask/urls.py
@@ -19,5 +19,5 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stats/', include('likes.urls'))
+    path('', include('likes.urls'))
 ]
--- goplustask/likes/models.py
+++ goplustask/likes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Stats(models.Model):
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)

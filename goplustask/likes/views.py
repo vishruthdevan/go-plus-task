5a0ac3c33bf60c639feb2c7b96101cf6aa131303
Vishruth Devan <vishruthdevan@gmail.com>
2023-01-21 17:02:55 +0530

    fix: create like and dislike fields on first visit

--- goplustask/likes/views.py
+++ goplustask/likes/views.py
@@ -10,9 +10,8 @@ def index(request):
     context = {}
     if not Stats.objects.all():
         Stats.objects.create(likes=0, dislikes=0)
-    else:
-        context["likes"] = Stats.objects.all()[0].likes
-        context["dislikes"] = Stats.objects.all()[0].dislikes
+    context["likes"] = Stats.objects.all()[0].likes
+    context["dislikes"] = Stats.objects.all()[0].dislikes
     return render(request, 'likes/index.html', context)
 
 

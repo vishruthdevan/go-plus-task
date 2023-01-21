bf5b79cded3ffaa690c5b9fc293b4f905cb688e0
Vishruth Devan <vishruthdevan@gmail.com>
2023-01-21 16:28:18 +0530

    feat: create endpoints to update likes and dislikes

--- goplustask/likes/views.py
+++ goplustask/likes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
 from .models import Stats
 # Create your views here.
 
@@ -12,3 +12,21 @@ def index(request):
         context["likes"] = Stats.objects.all()[0].likes
         context["dislikes"] = Stats.objects.all()[0].dislikes
     return render(request, 'likes/index.html', context)
+
+
+def like(request):
+    if not Stats.objects.all():
+        Stats.objects.create(likes=0, dislikes=0)
+    else:
+        Stats.objects.all()[0].likes += 1
+        Stats.objects.all()[0].save()
+    return JsonResponse({"likes": Stats.objects.all()[0].likes})
+
+
+def dislike(request):
+    if not Stats.objects.all():
+        Stats.objects.create(likes=0, dislikes=0)
+    else:
+        Stats.objects.all()[0].dislikes += 1
+        Stats.objects.all()[0].save()
+    return JsonResponse({"dislikes": Stats.objects.all()[0].dislikes})

4b2bbd3e66592bfae5e6db645b2989dc953ba158
Vishruth Devan <vishruthdevan@gmail.com>
2023-01-21 16:25:37 +0530

    chore: apply migrations

--- goplustask/likes/views.py
+++ goplustask/likes/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Stats
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'likes/index.html')
+    context = {}
+    if not Stats.objects.all():
+        Stats.objects.create(likes=0, dislikes=0)
+    else:
+        context["likes"] = Stats.objects.all()[0].likes
+        context["dislikes"] = Stats.objects.all()[0].dislikes
+    return render(request, 'likes/index.html', context)
